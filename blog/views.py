0a2335f24ee308a85414fd95cc47c8ea29e14b09
Rishi Khandelwal <rish860official@gmail.com>
2020-08-30 17:29:00 +0530

    nav added to the base template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author':'Rishi',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 30, 2020'
+    },
+    {
+        'author':'Dishi',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 30, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('Blog Home')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context )
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
