8a20310cf1a9d2af6c8175f9fbaa12c4bd711c38
Rishi Khandelwal <rish860official@gmail.com>
2020-08-31 19:09:43 +0530

    admin panel

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+#this auto_now_add will create the date_posted to the date it was createdif we remove 'add' it means will change every time we update it
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) # this is a function but we dont want it to be executes as a function
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'Rishi',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 30, 2020'
-    },
-    {
-        'author':'Dishi',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 30, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context )
 
