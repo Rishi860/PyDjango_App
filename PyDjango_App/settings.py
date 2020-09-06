94fe4f5948e7483ba84bf2419a205d9cb210b701
Rishi Khandelwal <rish860official@gmail.com>
2020-09-06 23:25:17 +0530

    env variables

--- PyDjango_App/settings.py
+++ PyDjango_App/settings.py
@@ -133,7 +133,7 @@ LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dusadd8@gmail.com'
-EMAIL_HOST_PASSWORD = 'lcfcrhovattllugf'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
