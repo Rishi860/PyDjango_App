f57b78dd77ad989aefd346c7f58688a972cb65a1
Rishi Khandelwal <rish860official@gmail.com>
2020-09-07 00:11:07 +0530

    Added django-heroku

--- PyDjango_App/settings.py
+++ PyDjango_App/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -139,3 +140,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+# automatically configures our database url and will also take care of connecting
+# are static assets to gunicorn using whitenoise
+django_heroku.settings(locals())
