5acc98a8c116ff97ff4ea1de8e6816364e185310
Rishi Khandelwal <rish860official@gmail.com>
2020-09-24 19:40:06 +0530

    Email fix

--- PyDjango_App/urls.py
+++ PyDjango_App/urls.py
@@ -34,11 +34,11 @@ urlpatterns = [
          auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
          name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
-         name='password_reset_complete'),
-    path('password-reset-complete/',
          auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
          name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
