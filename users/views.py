59c78fdf9a7560badd6480418974f42a753416da
Rishi Khandelwal <rish860official@gmail.com>
2020-10-25 18:39:20 +0530

    Email verification

--- users/views.py
+++ users/views.py
@@ -1,19 +1,62 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.http import HttpResponse
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_text
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
+from django.contrib.auth.models import User
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created, You can Login now!')
-            return redirect('login')
+            user = form.save(commit=False) # Saved the form in the form of a instance
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your User Account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            # print("hey i am here")
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+            # username = form.cleaned_data.get('username')
+            # messages.success(request, f'Your account has been created, You can Login now!')
+            return HttpResponse('Please confirm your email address to complete the registration')
+            # return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save() # saved the data of the form 
+        # login(request, user)
+        # return redirect('home')
+        return render(request, 'users/acc_confirmed.html', {'title': 'Account Confirmed'})
+        # return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        user.delete()
+        return HttpResponse('Activation link is invalid!')
 
 
 @login_required
